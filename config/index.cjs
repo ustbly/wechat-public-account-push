54631976493a3580a2ee306024aa387b939678ba
ustbly <74825952+ustbly@users.noreply.github.com>
2022-12-04 12:35:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o9g255uyJbyb-vikk1HiE2X7HsfU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '7UAWNpkvfSmMhwkVQVGGn4IGSPx66PvD87cQf58kdv4',
+      useTemplateId: 'aWKQ5GsZj5h96PLKYJwhytX1k45L6XDLml7oyEGegOk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-19',
       festivals: [
@@ -54,7 +54,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o9g255rQtpDCY8e1lsqToFSPP8vI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '7UAWNpkvfSmMhwkVQVGGn4IGSPx66PvD87cQf58kdv4',
+      useTemplateId: 'aWKQ5GsZj5h96PLKYJwhytX1k45L6XDLml7oyEGegOk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-17',
       festivals: [
@@ -75,7 +75,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  //CALLBACK_TEMPLATE_ID: '7UAWNpkvfSmMhwkVQVGGn4IGSPx66PvD87cQf58kdv4',
+  //CALLBACK_TEMPLATE_ID: 'aWKQ5GsZj5h96PLKYJwhytX1k45L6XDLml7oyEGegOk',
 
   CALLBACK_USERS: [
     {
