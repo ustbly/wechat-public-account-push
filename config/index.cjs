de0102bd2d1d37840ae8788176ced9d990e5fb65
ustbly <74825952+ustbly@users.noreply.github.com>
2022-12-04 11:21:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -14,7 +14,7 @@ const USER_CONFIG = {
   APP_SECRET: '625717b0a56ffb4bc2721dc988315339',
 
   PROVINCE: '山东',
-  CITY: '胶州',
+  CITY: '青岛',
 
   USERS: [
     {
@@ -30,6 +30,37 @@ const USER_CONFIG = {
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
           type: '*生日', name: '宝贝', year: '1997', date: '12-21',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        //{
+          //type: '生日', name: 'Vayne', year: '1998', date: '01-19',
+        //},
+        //{
+          //type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+        //},
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2021-12-01' },
+        // 结婚纪念日
+        //{ keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+    
+    {
+      // 想要发送的人的名字
+      name: '自己',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'o9g255rQtpDCY8e1lsqToFSPP8vI',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'JE6Mlht-B8ZcNwCprclOTYJnLB563ZbNGMc9y5IxK-0',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '01-19',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '胖虎', year: '1998', date: '09-29',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         //{
