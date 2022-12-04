dfe21c06fff592f8c7941829c623566c02555cda
ustbly <74825952+ustbly@users.noreply.github.com>
2022-12-04 12:13:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o9g255uyJbyb-vikk1HiE2X7HsfU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'JE6Mlht-B8ZcNwCprclOTYJnLB563ZbNGMc9y5IxK-0',
+      useTemplateId: 'l9_eeRoM7VxzqhSvX-24iV2i5lKjLUINEyHyOMQ4-SE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-19',
       festivals: [
@@ -82,7 +82,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'JTk71dM8otXwrBoWKRFhk69nEh0tBON3nUB-qiwZjaY',
+  CALLBACK_TEMPLATE_ID: 'l9_eeRoM7VxzqhSvX-24iV2i5lKjLUINEyHyOMQ4-SE',
 
   CALLBACK_USERS: [
     {
